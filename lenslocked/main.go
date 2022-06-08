fa64a19139acf867b2f4f2714c85a96ff8b6103a
Steve Skladany <steve@skladany.net>
2022-06-08 19:51:51 -0400

    docs: section 1 complete, section 36% complete

--- lenslocked/main.go
+++ lenslocked/main.go
@@ -5,12 +5,23 @@ import (
 	"net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Welcome to my awesome GO site!!!</h1>")
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Welcome to my awesome GO site dude!</h1>")
+}
+
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch email me at <a href=\"mailto:steve@skladany.net\">steve@skladany.net</a>")
+}
+
+func pathHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, r.URL.Path)
 }
 
 func main() {
-	http.HandleFunc("/", handlerFunc)
+	http.HandleFunc("/", pathHandler)
+	// http.HandleFunc("/contact", contactHandler)
 	fmt.Println("Starting the server on :3000...")
 	http.ListenAndServe(":3000", nil)
 }
--- lenslocked/readme.md
+++ lenslocked/readme.md
@@ -13,17 +13,17 @@ Following along with this great Go Tutorial:
 - [x] Editors and Automatic Imports
 - [x] The "Hello, World" Part of our Code
 - [x] Web Request
-- [ ] HTTP Methods
-- [ ] Our Handler Function
-- [ ] Registering our Handler Function and Starting the Web Server
-- [ ] Go Modules
+- [x] HTTP Methods
+- [x] Our Handler Function
+- [x] Registering our Handler Function and Starting the Web Server
+- [x] Go Modules
 
 ## Section 2- Adding New Pages
 
-- [ ] Dynamic Reloading
-- [ ] Setting Header Values
-- [ ] Creating a Contact Page
-- [ ] Examining the http.Request Type
+- [x] Dynamic Reloading
+- [x] Setting Header Values
+- [x] Creating a Contact Page
+- [x] Examining the http.Request Type
 - [ ] Custom Routing
 - [ ] url.Path vs url.RawPath
 - [ ] Not Found Page
